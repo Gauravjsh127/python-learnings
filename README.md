36aa4f10dcc4848cdc5bffd09bbcb2fa698bedcb
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-06 11:04:07 +0100

    added python list example

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Exploring algorithms and data structures problems and  solving them with python.
 
 
 ## Learning structure
-The python-learnings examples is composed by the following modules and each module have seperate README:
+The python-learnings examples are composed by the following modules and each module have seperate README:
 
 | modules                | Root directory            | README file                                |
 |------------------------|---------------------------|--------------------------------------------|
--- python-data-structures/list-example.py
+++ python-data-structures/list-example.py
@@ -1,10 +1,118 @@
-
 """
-
 Python List examples
 
+Summary
+
+Add    
+   - append(val) : to end of list
+   - insert(pos, val)
+
+Delete
+   - remove(val) : delete by value
+   - pop(pos optional): delete by position
+   - clear()  : delete complete list
+
+size 
+   - len(list)
+
+occurance
+   - count(val)
+
+sort/reverse
+    - sort
+    - reverse
+
 """
-# Creating a List 
-List = [5] 
-print("Intial blank List: ") 
-print(List) 
+
+class solution:
+    def __init__(self, mylist):
+        """
+        Initialization
+        """
+        self.list = mylist
+
+    def display_list(self):
+        """
+        Display list
+        """
+        print(self.list)
+
+    def append(self, elem):
+        """
+        Add an item to the end of the list
+        """
+        self.list.append(elem)
+
+    def insert(self, pos, elem):
+        """
+        Insert an item at a given position
+        """
+        self.list.insert(pos, elem)
+
+    def remove(self, val):
+        """
+        Remove the first item from the list whose value is equal to val.
+        """
+        self.list.remove(val)
+
+    def pop(self, pos):
+        """
+        Remove the item at the given position in the list, and return it. If no index is specified, a.pop() removes and returns the last item in the list
+        """
+        self.list.pop(pos)
+
+    def clear(self):
+        """
+        Remove all items from the list. Equivalent to del a[:].
+        """
+        self.list.clear()
+
+    def count(self, val):
+        """
+        Return the number of times x appears in the list.
+        """
+        return self.list.count(val)
+
+    def length(self):
+        """
+        Return the size of the list
+        """
+        return len(self.list)
+
+    def sort(self):
+        """
+        Sort the given list
+        """
+        self.list.sort()
+
+    def reverse(self):
+        """
+        Reverse the given list
+        """
+        self.list.reverse()
+
+
+if __name__ == "__main__":
+    mylist = [1, 2, 3]
+    soln = solution(mylist)
+    soln.display_list()
+    soln.append(4)
+    soln.display_list()
+    soln.insert(0, 5)
+    soln.display_list()
+    soln.remove(3)
+    soln.display_list()
+    soln.pop(2)
+    soln.display_list()
+    soln.clear()
+    soln.display_list()
+    soln.append(4)
+    soln.append(4)
+    soln.append(2)
+    soln.append(9)
+    print(soln.count(4))
+    print(soln.length())
+    soln.sort()
+    soln.display_list()
+    soln.reverse()
+    soln.display_list()
