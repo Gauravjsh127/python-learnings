81436c51f214fa0b254994e7f60435953fcba598
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-07 16:47:02 +0100

    added 1 problem with solution

---
+++ 2-arrays-leetcode/1-remove-duplicates.py
@@ -0,0 +1,43 @@
+"""
+Remove Duplicates from Sorted Array
+
+Source : 
+
+https://www.geeksforgeeks.org/remove-duplicates-sorted-array/
+"""
+
+
+class Solution(object):
+    
+    # Not Using extra space
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        size = len(nums)
+        if size == 0 or size == 1:
+            return size
+
+        # To store index of next
+        # unique element
+        j = 0
+
+        # Doing same as done
+        # in Method 1 Just
+        # maintaining another
+        # updated index i.e. j
+        for i in range(0, (size - 1)):
+            if nums[i] != nums[i + 1]:
+                nums[j] = nums[i]
+                j += 1
+
+        nums[j] = nums[size - 1]
+        j += 1
+        return j
+
+
+
+
+if __name__ == "__main__":
+
--- 2-arrays-leetcode/README.md
+++ 2-arrays-leetcode/README.md
@@ -2,6 +2,14 @@
 
 This modules provide sample python code solving the leet code problems related to Arrays
 
+# Category : Easy problems
 
 ## Problem 1
 
+Remove Duplicates from Sorted Array
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
