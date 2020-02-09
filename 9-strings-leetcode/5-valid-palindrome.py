5e9580705ffdf65c0c7a7c878159ca40407cb21d
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-09 13:07:34 +0100

    comments added

--- 9-strings-leetcode/5-valid-palindrome.py
+++ 9-strings-leetcode/5-valid-palindrome.py
@@ -18,9 +18,9 @@ class Solution(object):
         :rtype: bool
         """
         # Run loop from 0 to len/2  
-        for i in xrange(0, len(s)/2): 
+        for i in xrange(0, len(s) / 2):
             
-            if s[i] != s[len(s)-i-1]: 
+            if s[i] != s[len(s) - i - 1]:
                 return False
         return True
 
@@ -32,20 +32,18 @@ class Solution(object):
         l=len(s)
         if l<2:
             return True
-        
-        start=0
-        end=l-1
-        
-        while start<end:
-            while s[start].isalpha() and start<end:
-                start=start+1
-            while s[end].isalpha() and start<end:
-                end=end-1
-            if s[start].lower() != s[end].lower() and start<end:
+        start = 0
+        end = l - 1
+        while start < end:
+            while s[start].isalpha() and start < end:
+                start = start + 1
+            while s[end].isalpha() and start < end:
+                end = end - 1
+            if s[start].lower() != s[end].lower() and start < end:
                 return False
             else:
-                start=start+1
-                end=end-1
+                start = start + 1
+                end = end - 1
         return True
 
 if __name__ == "__main__":
