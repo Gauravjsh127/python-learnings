6e6a6293de003b6ab60c2a77351ff9fc7c4220cd
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-09 20:30:37 +0100

    added examples

--- 9-strings-leetcode/7-KMP-substr-in-str.py
+++ 9-strings-leetcode/7-KMP-substr-in-str.py
@@ -10,5 +10,54 @@ Source:
 
 """
 
+class Solution(object):
 
+    def computePrefix(self, s):
+        # time O(n)  space O(n)
+        slength=len(s)
+        prefix=[0]*slength   
+        prefix_index = 0
+        suffix_index = 1
+        while suffix_index < slength:
+            if s[suffix_index] == s[suffix_index]: 
+                prefix[suffix_index] = prefix_index + 1
+                prefix_index=prefix_index+1
+                suffix_index=suffix_index+1
+            else:
+                if prefix_index:
+                    prefix_index = prefix[prefix_index - 1]
+                else:
+                    suffix_index=suffix_index+1
+        return prefix
+
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # time O(m + n)     // space O(n)
+        if len(haystack) ==0:
+            return 0;
+        if len(needle) ==0:
+            return -1;
+
+        prefix=self.computePrefix(haystack)
+        i = 0
+        j = 0
+        m = len(haystack)
+        n = len(needle)
+        while (i < m):
+            if (haystack[i] == needle[j]):
+                i=i+1
+                j=j+1
+            else:
+                if j:
+                    j = prefix[j - 1]
+                else:
+                    i=i+1
+            if j == n:
+                return i - j
+        return -1
+        
 if __name__ == "__main__":
