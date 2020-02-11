d7e00291194a811ad8a62d2b2b68fef26d6b1c51
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-11 15:10:54 +0100

    Added sample example

---
+++ 7-design-leetcode/4-last-3-websites-visited.py
@@ -0,0 +1,84 @@
+"""
+Last 3 websites visited
+
+Example 1:  AABBBCCCCDDDDDDABBBBB 
+output : BAD
+"""
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LastKWebsiteVisited():
+    def __init__(self, size):
+        self.size = size
+        self.head = None    # New entry always added from begining / deletion can be from anywhere
+        self.wmap = {}      # key is the website name and value is the list index that stores that website
+
+    def add_at_begining(self, newdata):
+        newNode = Node(newdata)
+        newNode.next = self.head
+        self.head = newNode
+        return newNode
+
+    def del_at_end(self):
+        node = self.head
+        nextnode = node.next
+        while nextnode.next != None:
+            node = nextnode
+            nextnode = nextnode.next
+        node.next = None
+        val = nextnode.data
+        del nextnode
+        return val
+
+    def del_based_on_node(self, data):
+        #print('data :' + data)
+        node = self.head
+        if node == None:
+            return
+
+        ## Data matches with the very first node
+        if (node.data == data):
+            self.head = node.next
+            del node
+            return
+
+        prevnode = None
+        while node.data != data:
+            prevnode = node
+            node = node.next
+        prevnode.next = node.next
+        del node
+
+    def website_visited(self, website_visited_list):
+        for web in website_visited_list:
+            #print("New Web URL :" + web)
+            if web in self.wmap:
+                #print('Already in wmap')
+                node = self.wmap[web]
+                self.del_based_on_node(node.data)
+                self.wmap[web] = self.add_at_begining(web)
+            else:
+                #print('Not in wmap')
+                if len(self.wmap) == self.size:
+                    web_least_visited = self.del_at_end()
+                    del self.wmap[web_least_visited]
+                self.wmap[web] = self.add_at_begining(web)
+            #self.print_lask_k_list()
+
+    def print_lask_k_list(self):
+        print('Last K web URLS : ')
+        node = self.head
+        while node != None:
+            print(node.data)
+            node = node.next
+
+
+K = 2
+obj = LastKWebsiteVisited(K)
+WEB_URL_LIST = 'AABBBCCCCDDDDDDABBBBB'
+print(WEB_URL_LIST)
+obj.website_visited(WEB_URL_LIST)
+obj.print_lask_k_list()
--- 7-design-leetcode/README.md
+++ 7-design-leetcode/README.md
@@ -25,3 +25,20 @@ solution.shuffle();
 solution.reset();
 // Returns the random shuffling of array [1,2,3].
 solution.shuffle();
+
+
+## Problem 3 : Top k frequent words   ???????
+
+Top K Frequent WordsYour answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+Example 1: Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2 Output: ["i", "love"]
+Explanation: "i" and "love" are the two most frequent words. Note that "i" comes before "love" due to a lower alphabetical order.
+Example 2: Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+Output: ["the", "is", "sunny", "day"] Explanation: "the", "is", "sunny" and "day" are the four most frequent words,with the number of occurrence being 4, 3, 2 and 1 respectively.
+
+## Problem 4 : Last 3 websites visited
+
+Example 1:  AABBBCCCCDDDDDDABBBBB 
+output : BAD
+
+
