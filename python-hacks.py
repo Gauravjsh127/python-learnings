9542ec3ca1900737961972332d0e1234ae710841
Joshi Gaurav <joshi.gaurav@bcgplatinion.com>
2020-02-14 17:19:26 +0100

    added sorting example

--- python-hacks.py
+++ python-hacks.py
@@ -1,3 +1,19 @@
+
+
+# vowels list
+vowels = ['e', 'a', 'u', 'o', 'i']
+# sort the vowels
+vowels.sort()
+# print vowels
+print('Sorted list:', vowels)
+
+# vowels list
+vowels = ['e', 'a', 'u', 'o', 'i']
+# sort the vowels
+vowels.sort(reverse=True)
+# print vowels
+print('Sorted list (in Descending):', vowels)
+
 # initialize array
 
 # 1D
